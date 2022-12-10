07a9f896c943201d13f361fa3e336c299e5db178
viriv <viriv@protonmail.com>
2022-12-10 16:44:00 +0100

    remove useless and confusing comments

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,9 @@ import { activateEventListeners } from "./buttonListeners";
 import "./style.css";
 import { currentSum, result } from "./math";
 
-// import { setupCounter } from "./counter";
-
 export function updateDisplay() {
 	const sum = document.querySelector(".sum span")
 	const display = document.querySelector(".display span")
-	// entries should be displayed as reversed for rtl scrolling hack
 	sum.textContent = currentSum.join(" ")
 	if (currentSum.join(" ") === "") sum.textContent = "0"
 	display.textContent = result.toString()
