f5dd881b4303834e5eaa0ed45e96e94fc9ea8ad5
viriv <viriv@protonmail.com>
2023-01-13 23:01:56 +0100

    Add Comments

--- src/math.ts
+++ src/math.ts
@@ -3,14 +3,20 @@ import { updateDisplay } from "./main";
 export let currentSum = [];
 export let result: number | "Error" = 0;
 
+
+//filters out any empty entries in the currentSum array.
 export function deleteEmptyArrayEntries() {
 	currentSum = currentSum.filter((entry) => entry.trim() != "");
 }
 
+
+//assigns the value of the toAssign parameter to the currentSum array.
 export function backspaceAssign(toAssign) {
 	currentSum = toAssign;
 }
 
+
+//clears the currentSum array and, if specified, the result variable. It also calls the updateDisplay() function from the main module.
 export function clear(clearResult: boolean) {
 	if (clearResult) result = 0;
 	currentSum = [];
@@ -18,9 +24,11 @@ export function clear(clearResult: boolean) {
 	return;
 }
 
+
+//updates the result variable based on the input provided. If specified, it also copies the result to the currentSum array.
 export function updateResult(input, copyResultToCurrentSum?: boolean) {
 	if (!input.length) return;
-	copyResultToCurrentSum = false;
+	copyResultToCurrentSum = false ?? copyResultToCurrentSum;
 	if (input[0] && !input[0].toString().includes("-") && input.length === 1)
 		result = input[0];
 	if (input[0].toString().includes("-") && input.length === 1 && input[0])
@@ -35,6 +43,8 @@ export function updateResult(input, copyResultToCurrentSum?: boolean) {
 	currentSum.splice(0, 3);
 }
 
+
+//takes in an input array and performs the specified mathematical operation on the first and second elements of the array. If the operation results in a NaN, it returns "Error".
 export function executeOperation(input): number | "Error" {
 	const firstNum = parseFloat(input[0]);
 	const secondNum = parseFloat(input[2]);
